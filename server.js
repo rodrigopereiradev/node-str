0feffffdf94e1ee65e7bed375c8eb14460df1253
Rodrigo S. Pereira <rodrigo.pereira@aisdigital.com.br>
2019-02-26 20:02:00 -0300

    firt commit

---
+++ server.js
@@ -0,0 +1,73 @@
+
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+const express = require('express');
+
+const app = express();
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: 'Node Store API',
+        version: '0.0.1'
+    });
+});
+
+app.use('/', route);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+
+console.log('API rodando na porta ' + port);
+
+
+/**
+Function responsible for normalize the port used for server
+ */
+function normalizePort(value) {
+    const port = parseInt(value, 10);
+    if (isNaN(port))
+        return value;
+    if (port >= 0)
+        return port;
+    return false;
+}
+
+
+/**
+Funciton responsible for server errors treatment
+ */
+function onError(error) {
+    if (error.syscall !== 'listen')
+        throw error;
+
+    const bind = typeof port === 'string' ? 'Pipe'  + port : 'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Function responsible for debug
+ */
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    debug('Listening ' + bind)
+}
