497474480dbae4b2fda52a4922a8068c0fd4e362
Rodrigo Pereira <rodrigopereira.dev@gmai.com>
2019-03-17 18:55:31 -0300

    first endpoints

--- src/app.js
+++ src/app.js
@@ -1,16 +1,39 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: 'Node Store API',
-        version: '0.0.1'
+        version: '0.0.2'
     });
 });
 
+const create = router.post('/', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        id: id,
+        item: req.body
+    });
+});
+
+const remove = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body)
+});
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', remove);
 module.exports = app;
