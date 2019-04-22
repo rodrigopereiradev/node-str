4de446664c6e208338b1807c17ef37445ff18f1c
Rodrigo Pereira <rodrigopereira.dev@gmai.com>
2019-04-22 08:09:15 -0300

    creating routes and controller

--- src/app.js
+++ src/app.js
@@ -4,36 +4,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const router = express.Router();
+const router = express.Router()
+
+//Carrega as rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
-
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: 'Node Store API',
-        version: '0.0.2'
-    });
-});
-
-const create = router.post('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-const remove = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body)
-});
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', remove);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 module.exports = app;
