83296c77ea010c8cf08d3a24c057f4e0a90f8aea
Rodrigo Pereira <rodrigopereira.dev@gmai.com>
2019-05-01 11:28:03 -0300

    creating a database connection

--- src/app.js
+++ src/app.js
@@ -7,9 +7,19 @@ const mongoose = require('mongoose');
 const app = express();
 const router = express.Router();
 
+const USER_DATABASE = 'username';
+const PASSWORD_DATABASE = 'password';
+const URI_DATABASE = 'mongodb+srv://'+ USER_DATABASE + ':' + PASSWORD_DATABASE + '@cluster0-nyatq.mongodb.net/nodestr?retryWrites=true';
+const URI_DATABASE_LOCAL = 'mongodb://localhost:27017/node-str';
+
 //conecta ao banco
-mongoose.connect('mongodb+srv://rodrigopereiradev:#Coltm4a1@cluster0-nyatq.mongodb.net/nodestr?retryWrites=true',
-    { useNewUrlParser: true });
+mongoose.connect(URI_DATABASE_LOCAL, { useNewUrlParser: true })
+    .then(() => {
+        console.log('Database connected...');
+    }, err => {
+        console.log(URI_DATABASE);
+        console.log('Error on database connection: ' + err)
+    });
 
 //Carrega as rotas
 const indexRoute = require('./routes/index-route');
